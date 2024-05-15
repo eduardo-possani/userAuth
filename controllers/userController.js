b2bddad9bce0e9bad39bf4b2c5087eff119dcefc
eduardo-possani <possaniduardo@gmail.com>
2024-05-15 20:04:45 -0300

    fix: remove models import

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,13 @@
 const bcrypt = require('bcrypt');
-const models = require('../models/allImportModels')
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user')
 
 const createUser = async (req, res) => {
   const { name, email, password } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    await models.user.create({ name, email, password: hashedPassword });
+    await User.create({ name, email, password: hashedPassword });
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -15,7 +17,7 @@ const createUser = async (req, res) => {
 const login = async (req , res) =>{
     const { email, password } = req.body;
     try {
-      const user = await models.user.findOne({ where: { email } });
+      const user = await User.findOne({ where: { email } });
       if (!user) {
         throw new Error('User not found');
       }
@@ -32,11 +34,11 @@ const login = async (req , res) =>{
 
 const list = async (req, res) =>{
     try {
-        const users = await models.user.findAll();
-        res.json(users);
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      }
+      const users = await User.findAll();
+      res.json(users);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
 }
 
 module.exports = {
